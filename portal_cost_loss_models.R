d98de3aec9932c07ac52abb7dfa9f62335611d4a
Shawn Taylor <shawntaylor@weecology.org>
2017-10-09 11:47:44 -0400

    put different spp and thresholds in cost loss models

--- portal_cost_loss_models.R
+++ portal_cost_loss_models.R
@@ -6,25 +6,37 @@ library(tidyverse)
 
 temporal_scales = c(1,2,4,6)
 
-upper_threshold = 1000
-lower_threshold = 140
+# upper_threshold = 1000
+# lower_threshold = 10
 
-predictions = read_csv('results/portal_model_output.csv') 
+population_thresholds = c(5,10,20,40)
+
+predictions = read_csv('results/portal_model_output.csv') %>%
+  filter(!model %in% c('tsglm','awesome_model'))
 temporal_model_sets = read.csv('temporal_model_sets.csv')
 
 
 #x = predictions %>% gather(value_type, value, num_rodents, prediction)
 #ggplot(x, aes(x=project_month, y=value, group=value_type, color=value_type)) + geom_line() + facet_wrap(~initial_month)
 
-#ggplot(predictions, aes(x=project_month, y=prediction, group=initial_month, color=initial_month)) +
-#  geom_line() +
-#  #geom_hline(yintercept = upper_threshold, linetype=2) +
-#  geom_hline(yintercept = lower_threshold, linetype=2) +
-#  geom_line(aes(x=project_month, y=num_rodents), color='red', inherit.aes = FALSE)
+#A lead time vs error graph
+# predictions$lead_time = with(predictions, project_month - initial_month)
+# errors = predictions %>%
+#   group_by(lead_time, model) %>%
+#   summarise(rmse = sqrt(mean((prediction - num_rodents)^2))) %>%
+#   ungroup()
+# ggplot(errors, aes(x=lead_time, y=rmse, color=model)) + geom_line()
+
+ggplot(predictions, aes(x=project_month, y=prediction, group=initial_month, color=initial_month)) +
+ geom_line() +
+ #geom_hline(yintercept = upper_threshold, linetype=2) +
+ geom_hline(yintercept = lower_threshold, linetype=2) +
+ geom_line(aes(x=project_month, y=num_rodents), color='red', inherit.aes = FALSE) +
+ facet_wrap(species~model, ncol=2, scales='free_y')
 
 #Use replicate_month to order the timeseries within each forecast replicate
 predictions = predictions %>%
-  group_by(initial_month, model) %>%
+  group_by(initial_month, model, species) %>%
   arrange(project_month) %>%
   mutate(replicate_month = 1:12) %>%
   ungroup() %>%
@@ -33,20 +45,16 @@ predictions = predictions %>%
 
 
 
-predictions_binary = predictions %>%
-    mutate(observed_binary = (num_rodents < upper_threshold & num_rodents > lower_threshold)*1,
-          predicted_binary= (prediction < upper_threshold & prediction > lower_threshold)*1) %>%
-  # mutate(observed_binary = ( num_rodents > lower_threshold)*1,
-  #       predicted_binary= ( prediction > lower_threshold)*1) %>%
-  select(initial_month, replicate_month, observed_binary, predicted_binary, model)
+# predictions_binary = predictions %>%
+#     mutate(observed_binary = (num_rodents < upper_threshold & num_rodents > lower_threshold)*1,
+#           predicted_binary= (prediction < upper_threshold & prediction > lower_threshold)*1) %>%
+#   # mutate(observed_binary = ( num_rodents > lower_threshold)*1,
+#   #       predicted_binary= ( prediction > lower_threshold)*1) %>%
+#   select(initial_month, replicate_month, observed_binary, predicted_binary, model)
+
 
 ###########################################
-scaled_predictions = temporal_model_sets %>%
-  left_join(predictions_binary, by='replicate_month') %>%
-  group_by(model, initial_month, temporal_scale, temporal_cell_id) %>%
-  summarise(observed_binary = max(observed_binary),
-            predicted_binary = max(predicted_binary)) %>%
-  ungroup()
+
 
 #The per month 
 treatment_cost = 10
@@ -57,8 +65,9 @@ outcomes = data.frame(observed_binary=c(0,1,1,0),
                       predicted_binary=c(1,1,0,0),
                       type=c('fp','tp','fn','tn'))
 
+
 #Gives cost per yr in terms of prescribed treatment vs actual losses
-calculate_cost = function(df, treatment_cost, loss_cost, expense_type){
+calculate_expense = function(df, treatment_cost, loss_cost, expense_type){
   temporal_scale = unique(df$temporal_scale)
   
   if(expense_type=='perfect'){
@@ -77,8 +86,8 @@ calculate_cost = function(df, treatment_cost, loss_cost, expense_type){
     left_join(outcomes, by=c('observed_binary','predicted_binary')) %>%
     ungroup()
   
-  total_tp_fp_months = sum(df$type %in% c('fp','tp')) * temporal_scale
-  total_fn_months     = sum(df$type == 'fn') * temporal_scale
+  total_tp_fp_months = sum(df$type %in% c('fp','tp'))
+  total_fn_months     = sum(df$type == 'fn')
   total_cost = (treatment_cost * total_tp_fp_months) + (loss_cost * total_fn_months)
   
   total_replicates = df %>%
@@ -86,52 +95,81 @@ calculate_cost = function(df, treatment_cost, loss_cost, expense_type){
     distinct() %>%
     nrow()
   
-  total_monthly_replicates = total_replicates * temporal_scale
-  print(paste(total_monthly_replicates))
+  #total_monthly_replicates = total_replicates * temporal_scale
+  #print(paste(total_monthly_replicates))
   
-  average_per_month_cost = total_cost / total_monthly_replicates
+  average_per_month_cost = total_cost / total_replicates
   return(average_per_month_cost)  
 }
 
 
 ###############################################################################
-#Calculate cost/loss model curve
+#Calculate cost/loss model curves
 cost_loss_values=data.frame()
-#for(this_transect in unique(scaled_predictions_binary$Transect)){
-for(this_model in unique(scaled_predictions$model)){
-  for(this_loss_cost in possible_loss_costs){
-    smallest_grain_data = scaled_predictions %>%
-      filter(temporal_scale == min(temporal_scales), model==this_model)
+for(this_species in unique(predictions$species)){
+  for(this_threshold in population_thresholds){
+    predictions_binary = predictions %>%
+      filter(species==this_species) %>%
+      mutate(observed_binary = (num_rodents > this_threshold)*1,
+             predicted_binary= (prediction > this_threshold)*1) %>%
+      select(initial_month, replicate_month, observed_binary, predicted_binary, model)
     
-    smallest_grain_perfect = calculate_cost(smallest_grain_data, treatment_cost = treatment_cost, 
-                                            loss_cost = this_loss_cost, expense_type='perfect')
-    smallest_grain_cost_never = calculate_cost(smallest_grain_data, treatment_cost = treatment_cost, 
-                                               loss_cost = this_loss_cost, expense_type='never')
-    smallest_grain_cost_maximimum = min(treatment_cost, smallest_grain_cost_never)
-    print(smallest_grain_perfect)
-    print(smallest_grain_cost_maximimum)
-    for(this_temporal_scale in temporal_scales){
-      this_scale_data = scaled_predictions %>%
-        filter(temporal_scale == this_temporal_scale, model==this_model)
+    scaled_predictions = temporal_model_sets %>%
+      left_join(predictions_binary, by='replicate_month') %>%
+      group_by(model, initial_month, temporal_scale, temporal_cell_id) %>%
+      mutate(predicted_binary = max(predicted_binary)) %>%
+      ungroup()
+    
+    smallest_grain_data = scaled_predictions %>%
+      filter(temporal_scale == min(temporal_scales))
+    
+    for(this_loss_cost in possible_loss_costs){
+      smallest_grain_perfect = calculate_expense(smallest_grain_data, treatment_cost = treatment_cost, 
+                                              loss_cost = this_loss_cost, expense_type='perfect')
+      smallest_grain_cost_never = calculate_expense(smallest_grain_data, treatment_cost = treatment_cost, 
+                                                 loss_cost = this_loss_cost, expense_type='never')
+      smallest_grain_cost_maximimum = min(treatment_cost, smallest_grain_cost_never)
+      print(smallest_grain_perfect)
+      print(smallest_grain_cost_maximimum)
       
-      this_scale_expense = calculate_cost(this_scale_data, treatment_cost = treatment_cost, 
-                                          loss_cost = this_loss_cost, expense_type='forecast')
-      
-      cost_loss_values = cost_loss_values %>%
-        bind_rows(data.frame('a' = treatment_cost / this_loss_cost,
-                             'expense_max' = smallest_grain_cost_maximimum,
-                             'expense_perfect' = smallest_grain_perfect, 
-                             'expense_forecast' = this_scale_expense,
-                             'temporal_scale' = this_temporal_scale,
-                             'model' = this_model))
+      for(this_temporal_scale in temporal_scales){
+        this_scale_data = scaled_predictions %>%
+          filter(temporal_scale == this_temporal_scale)
+        
+        this_scale_expense = calculate_expense(this_scale_data, treatment_cost = treatment_cost, 
+                                            loss_cost = this_loss_cost, expense_type='forecast')
+        
+        cost_loss_values = cost_loss_values %>%
+          bind_rows(data.frame('a' = treatment_cost / this_loss_cost,
+                               'expense_max' = smallest_grain_cost_maximimum,
+                               'expense_perfect' = smallest_grain_perfect, 
+                               'expense_forecast' = this_scale_expense,
+                               'temporal_scale' = this_temporal_scale,
+                               'species' = this_species,
+                               'threshold' = this_threshold))
+      }
     }
-      
   }
 }
 cost_loss_values$value = with(cost_loss_values, (expense_max - expense_forecast)/(expense_max - expense_perfect))
 
 
 ggplot(cost_loss_values, aes(x=a, y=value, group=as.factor(temporal_scale), color=as.factor(temporal_scale))) +
-  geom_line() + 
+  geom_line(size=1.5) + 
   ylim(0,1) +
-  facet_grid(~model)
+  facet_grid(species~threshold) 
+
++ 
+  theme(plot.title = element_text(size = 30),
+        axis.text = element_text(size = 20),
+        axis.title = element_text(size = 22),
+        legend.text = element_text(size = 15), 
+        legend.title = element_text(size = 20),
+        strip.text.x=element_text(size=22),
+        strip.text.y=element_text(size=22),
+        legend.position = "bottom", 
+        legend.direction = "horizontal",
+        legend.key.width = unit(15, units = 'mm'),
+        legend) +
+  labs(title = "Portal Cost Loss Analysis",
+       color = 'Temporal Grain')
